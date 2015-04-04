69a6a9c896a01a203a62353fcef651d52d76a886
janith <janithasen@gmail.com>
2015-04-03 23:16:56 -0700

    added image response

--- src/main/java/rest.com/HelloWorldService.java
+++ src/main/java/rest.com/HelloWorldService.java
@@ -5,9 +5,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import java.io.File;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
 @Path("/hello")
 public class HelloWorldService {
- 
+
+	private static final String FILE_PATH = "C:\\image.jpg";
+
 	@GET
 	@Path("/{param}")
 	public Response getMsg(@PathParam("param") String msg) {
@@ -17,5 +23,19 @@ public class HelloWorldService {
 		return Response.status(200).entity(output).build();
  
 	}
- 
+
+	@GET
+	@Path("/get")
+	@Produces("image/jpg")
+	public Response getFile() {
+
+		File file = new File(FILE_PATH);
+
+		ResponseBuilder response = Response.ok((Object) file);
+		response.header("Content-Disposition",
+				"attachment; filename=image_from_server.png");
+		return response.build();
+
+	}
 }
+
